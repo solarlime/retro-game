3ffc98f7b37a99f1c3350ffca48bed819c96175c
Solarlime <solarlime@outlook.com>
2020-02-04 20:22:40 +0300

    Changed for passing tests

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,6 +1,7 @@
-export default function tooltip(character) {
-  return `${String.fromCodePoint(0x1F396)}${character.level} 
-        ${String.fromCodePoint(0x2694)}${character.attack} 
-        ${String.fromCodePoint(0x1F6E1)}${character.defence} 
-        ${String.fromCodePoint(0x2764)}${character.health}`;
+export default function tooltip(item) {
+  const level = String.fromCodePoint(0x1F396);
+  const attack = String.fromCodePoint(0x2694);
+  const defence = String.fromCodePoint(0x1F6E1);
+  const health = String.fromCodePoint(0x2764);
+  return `${level}${item.level} ${attack}${item.attack} ${defence}${item.defence} ${health}${item.health}`;
 }
