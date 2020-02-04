320dd451874d8f00911aa4902a420135550812d1
Solarlime <solarlime@outlook.com>
2020-02-04 19:54:57 +0300

    Added tooltips for warriors

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,3 +1,10 @@
+import GamePlay from './GamePlay';
+import themes from './themes';
+import PositionedCharacter from "./PositionedCharacter";
+
+let lightSide = [];
+let darkSide = [];
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
@@ -5,8 +12,12 @@ export default class GameController {
   }
 
   init() {
+    this.gamePlay.drawUi(themes.prairie);
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+    this.gamePlay.addCellEnterListener(this.onCellEnter);
+    this.gamePlay.addCellClickListener(this.onCellClick);
+    this.gamePlay.addCellLeaveListener(this.onCellLeave);
   }
 
   onCellClick(index) {
@@ -15,9 +26,20 @@ export default class GameController {
 
   onCellEnter(index) {
     // TODO: react to mouse enter
+    [...lightSide, ...darkSide].forEach((item) => {
+      item.position === index ? this.gamePlay.showCellTooltip(
+        `${String.fromCodePoint(0x1F396)}${item.character.level} 
+        ${String.fromCodePoint(0x2694)}${item.character.attack} 
+        ${String.fromCodePoint(0x1F6E1)}${item.character.defence} 
+        ${String.fromCodePoint(0x2764)}${item.character.health}`,
+        index,
+      ) : null;
+    });
+
   }
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+    this.gamePlay.hideCellTooltip(index);
   }
 }
